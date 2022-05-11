af276d91143494757e09789e1b39bfbacd5ebd2f
SUNGWHANYOON <35682200+SUNGWHANYOON@users.noreply.github.com>
2022-05-11 19:25:46 +0900

    Delete TapModel.swift

--- KNU Museum/Shared/TapModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum Tap: String,CaseIterable{
-    case bookmark = "book"
-    case time = "clock"
-    case camera = "camera"
-    case chat = "questionmark"
-   case setting = "pencil"
-}
