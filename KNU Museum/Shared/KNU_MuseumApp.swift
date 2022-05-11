f2c10028591f5177d31a7c883c71fc719c099901
Sreya8 <54528672+Sreya8@users.noreply.github.com>
2022-05-11 20:03:24 +0900

    Added Name To Card

---
+++ KNU Museum/Shared/KNU_MuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  KNU_MuseumApp.swift
+//  Shared
+//
+//  Created by Sreya Reddy on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct KNU_MuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
