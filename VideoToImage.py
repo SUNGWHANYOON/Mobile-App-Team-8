094b36ade8f14797a06d9666973ac26706206a2f
Sreya8 <54528672+Sreya8@users.noreply.github.com>
2022-05-16 05:51:57 +0900

    Update VideoToImage.py

--- VideoToImage.py
+++ VideoToImage.py
@@ -1,21 +1,28 @@
 import cv2
 import os
+import imutils
 
-vidcap = cv2.VideoCapture('/Users/sreyareddy/Downloads/All Videos/12.MOV')
-save_path = '/Users/sreyareddy/Downloads/Images/Object 1'
+vidcap = cv2.VideoCapture('/Users/sreyareddy/Developer/Python/KNU Museum/All Videos/102.MOV')
+save_path = '/Users/sreyareddy/Developer/Python/KNU Museum/Images/Train/10'
 
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        filename = "(12)"+str(count)+".jpg"
+        filename = "(102)"+str(count)+".jpg"
         completeName = os.path.join(save_path, filename)
-        cv2.imwrite(completeName, image)     # save frame as JPG file
+        try:
+            resized = imutils.resize(image, width=200)
+            img = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+
+            cv2.imwrite(completeName, img)  # save frame as JPG file
+        except:
+            print("Not Working")
+
     return hasFrames
 
-
 sec = 0
-frameRate = 0.1  # //it will capture image in each 0.1 second
+frameRate = 0.5  # //it will capture image in each 0.5 second
 count = 1
 success = getFrame(sec)
 while success:
@@ -23,4 +30,3 @@ while success:
     sec = sec + frameRate
     sec = round(sec, 2)
     success = getFrame(sec)
-
