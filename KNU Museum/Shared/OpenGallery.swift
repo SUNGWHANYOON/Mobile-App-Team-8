f29419f7512a50b726a7875c0922e7be67cf66ee
윤성환 <tjdghks0214@gmail.com>
2022-05-15 20:33:30 +0900

    OpenGallery

--- KNU Museum/Shared/OpenGallery.swift
+++ KNU Museum/Shared/OpenGallery.swift
@@ -5,4 +5,55 @@
 //  Created by 윤성환 on 2022/05/15.
 //
 
+import SwiftUI
 import Foundation
+import UIKit
+
+struct ImagePickerView: UIViewControllerRepresentable{
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>)-> UIImagePickerController{
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+        //context
+    }
+}
+
+struct OpenGallery: View{
+    
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
+    var body: some View{
+        VStack{
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack{
+                    Image(systemName : "photo")
+                        .font(.system(size:20))
+                    
+                    Text("Photo library")
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, idealWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }
+    }
+}
